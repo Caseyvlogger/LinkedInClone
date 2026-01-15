6d284fcfaa15f3f64f15529c46e16c5426dcbe53
Caseyvlogger <ahmedkhan123101@gmail.com>
2026-01-15 09:14:04 +0500

    installation: react-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home.jsx"
+import Feed from "./pages/Feed.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import SignIn from "./pages/SignIn.jsx"
+
 function App() {
 
   return (
-    <div>
-      <Home/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+    </Routes>
   )
 }
 

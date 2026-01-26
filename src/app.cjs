3d1f55687c87ffb5d73dc1b9d1a710903fefec83
Caseyvlogger <ahmedkhan123101@gmail.com>
2026-01-26 10:29:22 +0500

    installation: mongoose.

--- src/app.cjs
+++ src/app.cjs
@@ -1,11 +1,19 @@
-const express=require('express');
-const v1Routes=require('./routes/v1/auth.route.cjs')
+const express = require('express');
+const mongoose = require('mongoose');
+const v1Routes = require('./routes/v1/auth.route.cjs');
 
-const app=express();
+const app = express();
 
-app.use(express.json())
-app.use('/v1', v1Routes)
+app.use(express.json());
 
-app.listen(3000, ()=>{
+// Connect to MongoDB 
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+  .then(() => console.log('Connected!'))
+  .catch(err => console.error('Connection error:', err));
+
+app.use('/v1', v1Routes);
+
+app.listen(3000, () => {
     console.log(`LinkedInClone server listening on http://localhost:3000`);
-})
+});

8d508a1b5dbf57d29cf1fbda3238c7d904f8b864
Caseyvlogger <ahmedkhan123101@gmail.com>
2026-01-30 15:04:37 +0500

    wip: avatar placeholder.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -65,9 +65,21 @@ function Profile() {
                 {/* Profile div: w-[804px]; h-201 <1200px; w-636 h-159; <922px: 396, 99; <768px: 576, 144 */}
                 <div className="min-[768px]:w-[396px] min-[922px]:w-[636px] min-[1200px]:w-[804px] border md:mr-5 relative">
                     {/* bg banner */}
-                    <div className="w-full bg-gray-300 h-[201px]">
-                        {/* <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by kmg design - Flaticon</a> */}
-                            
+                    <div className="w-full bg-gray-300 
+                                    h-[30vw] 
+                                    min-[600px]:h-[120px]
+                                    md:h-[144px] 
+                                    min-[992px]:h-[99px] 
+                                    xl:h-[159px] 
+                                    min-[1400px]:h-[201px]
+                                    ">
+                        {/* Profile Placeholder + '+' button */}
+                        <div>
+                            <img className="absolute border top-17" src="src/assets/profile-placeholder-128.png" alt="Profile placeholder" />
+                            <div className="bg-white border border-[#0a66c2] h-[40px] w-[40px] rounded-full">
+                                <span className="text-[#0a66c2] text-[30px] absolute">+</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {/* Profile language + Settings/People col */}

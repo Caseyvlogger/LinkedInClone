f97c9595fe62b1c7c90958a133f3d2e3000c9069
Caseyvlogger <ahmedkhan123101@gmail.com>
2026-02-11 11:29:25 +0500

    Moved backend outside.

--- src/backend/validations/custom.validation.cjs
+++
@@ -1,8 +0,0 @@
-const password = (value, helpers) => {
-    if (value.length < 6) {
-        return helpers.message('Password must be at least 6 characters');
-    }
-    return value;
-};
-
-module.exports = { password, };
